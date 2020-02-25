a361d901ffc2481e18eee157a31ee74210948979
“sunkuangdong <sunkuangdong@ele-cloud.com>
2020-02-25 21:07:31 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import VueRouter from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
-// import Home from '../views/Home.vue';
+import NotFound from '@/views/NotFound.vue';
+
 
 Vue.use(VueRouter);
 
@@ -23,20 +24,11 @@ const routes = [
     {
         path: '/statistics',
         component: Statistics
+    },
+    {
+        path: '*',
+        component: NotFound
     }
-    // {
-    //     path: '/',
-    //     name: 'Home',
-    //     component: Home
-    // },
-    // {
-    //     path: '/about',
-    //     name: 'About',
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
 ];
 
 const router = new VueRouter({
