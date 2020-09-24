4afd60390165ca9356f813d143702c3c86439c64
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-09-24 10:16:54 +0800

    基本图表

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -88,6 +88,11 @@ export default class Statistics extends Vue {
         data: keys,
         axisTick: { alignWithLabel: true },
         axisLine: { lineStyle: { color: "#666" } },
+        axisLabel: {
+          formatter: function (value: string, index: number) {
+            return value.substr(5);
+          },
+        },
       },
       yAxis: {
         type: "value",
